8b4f912b162a3f429539ce44f39737018527d03b
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2024-02-08 15:52:08 -0800

    Created general Style Sheet and header.js

---
+++ UBC Forums/webapp/scripts/header.js
@@ -0,0 +1,14 @@
+document.write(`
+    <div>
+        <a href="index.html">
+            <img src="../images/logo.png" alt="UBC Forums">
+        </a>
+        <form action=''>
+            <input type='text' placeholder='Search'>
+        </form>
+        <a href="">Dashboard</a>
+        <a href="">Forums</a>
+        <a id = 'user' href=""><img alt = 'Profile'src = ''></a>
+        <a class = "hidden" href="login.html">Login</a>
+    </div>
+`);
