ebc09cf71634c92d3b23d4e9d4a9bba1cc9eacc3
joyfuluj <joyumejiego@yahoo.com>
2024-03-23 12:25:52 -0700

    limited type of files user can upload

--- UBC Forums/database/database.ddl
+++ UBC Forums/database/database.ddl
@@ -1,6 +1,7 @@
+CREATE DATABASE IF NOT EXISTS db_81265373;
 USE db_81265373;
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     userId INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(30),
     password VARCHAR(25),
@@ -9,18 +10,55 @@ CREATE TABLE users (
     lastName VARCHAR(25),
     signUpDate DATETIME
 );
-);
 
-CREATE TABLE community (
-    communityID INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS community (
+    communityId INT AUTO_INCREMENT PRIMARY KEY,
     communityName VARCHAR(20) UNIQUE,
     communityDesc VARCHAR(200),
     ownerId INT NOT NULL,
     FOREIGN KEY (ownerId) REFERENCES users(userId)
 );
+
+
+CREATE TABLE IF NOT EXISTS memberOf (
+    communityId INT,
+    userId INT,
+    type ENUM('member', 'moderator'),
+    joinDate DATETIME DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (communityId, userId),
+    FOREIGN KEY (userId) REFERENCES users(userId),
+    FOREIGN KEY (communityId) REFERENCES community(communityId)
+);
+
+CREATE TABLE IF NOT EXISTS posts (
+    postId INT AUTO_INCREMENT PRIMARY KEY,
+    postTitle VARCHAR(200),
+    communityId INT,
+    userId INT,
+    promos INT,
+    postType VARCHAR(10), 
+    postTime DATETIME,
+    FOREIGN KEY (userId) REFERENCES users(userId),
+    FOREIGN KEY (communityId) REFERENCES community(communityId),
+    UNIQUE(postId, communityId)
+);
+
+CREATE TABLE IF NOT EXISTS comments(
+    commentId INT AUTO_INCREMENT PRIMARY KEY,
+    postId INT,
+    communityId INT,
+    commentContent VARCHAR(900),
+    commentTime DATETIME,
+    promos INT,
+    userId INT,
+    FOREIGN KEY (postId) REFERENCES posts(postId),
+    FOREIGN KEY (userId) REFERENCES users(userId),
+    FOREIGN KEY (communityId) REFERENCES community(communityId),
+    UNIQUE (postId, communityId, commentId)
 );
 
 DELIMITER //
+/*
 CREATE TRIGGER newOwner
 AFTER DELETE ON users
 FOR EACH ROW
@@ -29,54 +67,31 @@ BEGIN
     SET ownerId = (
         SELECT userId
         FROM memberOf
-        WHERE communityID = communityID AND type = 'moderator'
+        WHERE communityId = communityId AND type = 'moderator'
         ORDER BY joinDate ASC
         LIMIT 1
     )
     WHERE ownerId = OLD.userId;
 END//
+*/
 DELIMITER ;
 
-CREATE TABLE memberOf (
-    communityID INT,
-    userId INT,
-    type ENUM('member', 'moderator'),
-    joinDate DATETIME DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (communityID, userId),
-    FOREIGN KEY (userId) REFERENCES users(userId),
-    FOREIGN KEY (communityID) REFERENCES community(communityID)
-);
-);
-
-CREATE TABLE posts (
-    postId INT AUTO_INCREMENT,
-    postTitle VARCHAR(200),
-    communityId INT,
-    userId INT,
-    promos INT,
-    postType VARCHAR(10), 
-    postTime DATETIME
-    FOREIGN KEY (userId) REFERENCES users(userId).
-    FOREIGN KEY (communityID) REFERENCES community(communityID)
-);
-
-CREATE TABLE comments (
-    commentId INT AUTO_INCREMENT PRIMARY KEY,
-    postId INT,
-    commentContent VARCHAR(900),
-    commentTime DATETIME,
-    promos INT,
-    userId INT,
-    FOREIGN KEY (postId) REFERENCES posts(postId),
-    FOREIGN KEY (userId) REFERENCES users(userId)
-);
-
-INSERT INTO `community` (`communityID`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'all', NULL,1);
-INSERT INTO `community` (`communityID`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'travel', NULL,1);
-INSERT INTO `community` (`communityID`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'game', NULL,1);
-INSERT INTO `community` (`communityID`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'al', NULL, 1);
-INSERT INTO `community` (`communityID`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'school', NULL,1);
-INSERT INTO `community` (`communityID`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'sports', NULL,1);
-
+DELIMITER //
+/*
+CREATE TRIGGER addOwnerAsModerator
+AFTER INSERT ON community
+FOR EACH ROW
+BEGIN
+    INSERT INTO memberOf (communityId, userId, type, joinDate)
+    VALUES (NEW.communityId, NEW.ownerId, 'moderator', NOW);
+END //
+*/
+DELIMITER ;
 
 INSERT INTO `users` (`userId`, `username`, `password`, `email`, `firstName`, `lastName`, `signUpDate`) VALUES (NULL, 'bob328', 'bobob', 'bob@gmail.com', 'Bob', 'BB', NOW());
+
+INSERT INTO `community` (`communityId`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'Travel', NULL,1);
+INSERT INTO `community` (`communityId`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'Game', NULL,1);
+INSERT INTO `community` (`communityId`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'Nature', NULL, 1);
+INSERT INTO `community` (`communityId`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'School', NULL,1);
+INSERT INTO `community` (`communityId`, `communityName`, `communityDesc`, `ownerId`) VALUES (NULL, 'Sports', NULL,1);
--- UBC Forums/webapp/pages/insert.php
+++ UBC Forums/webapp/pages/insert.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
     // $servername = $host;
     // $username = $user;
     // $password = $pass;
@@ -20,74 +23,96 @@
     $targetFile = $_FILES["image"]["name"];
     if (isset($_FILES["image"]["name"]) && $_FILES["image"]["error"] == 0) {
         echo "<br>file name is: ".$_FILES["image"]["name"];
+        // limity the type of the file
+        $validExt = array("jpg", "png");
+        $validMime = array("image/jpeg", "image/png") ;
+        foreach($_FILES as $fileKey => $fileArray ){
+            $parts = explode(".", $fileArray["name"]);
+            $extension = end($parts);
+            if (in_array($fileArray["type"],$validMime) &&
+                in_array($extension, $validExt)) {
+                echo "all is well. Extension and mime types valid";
+            }
+            else {
+                echo $fileKey." Has an invalid mime type or extension";
+                header("Location: post.php?invalid");
+                exit();
+            }
+        }
     } 
+    
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $communityName = $_POST["communities"];
         $postDesc = $_POST["postDesc"];
         if ($postDesc || $targetFile) {
-            $sql = "SELECT communityID FROM community WHERE communityName=?";
+            $sql = "SELECT communityId FROM community WHERE communityName = ?";
             if ($statement = mysqli_prepare($conn, $sql)) {
                 mysqli_stmt_bind_param($statement, 's', $communityName);
                 mysqli_stmt_execute($statement);
-                $result = mysqli_stmt_get_result($statement);
-                if ($row = mysqli_fetch_assoc($result)) {
-                    $communityID = $row['communityID'];
-                    $postDesc = $_POST["postDesc"];        
-                    $sql = "INSERT INTO posts (communityID, userId, postTime) VALUES (?, 1, NOW())";
-                    if ($statement = mysqli_prepare($conn, $sql)) {
-                        mysqli_stmt_bind_param($statement, 's', $communityID);
-                        mysqli_stmt_execute($statement);
-                        $postType="";
-                        $fileName="";
-                        $postID = mysqli_insert_id($conn);
-                        if($postDesc){
-                            $fileName = "$communityID" . "-" . "$postID.txt";
-                            $postType="text";
-                            $filePath = "../posts/$fileName";
-                            //Upload Text post
-                            $newFile = fopen("$filePath", "w");
-                            if ($newFile) {
-                                fwrite($newFile, $postDesc);
-                                fclose($newFile);
-                                echo "Post created!";
-                            } else {
-                                echo "Error creating file!";
-                            }
-                            $fileName = "$communityID" . "-" . "$postID.txt";
-                        } 
-                        else{
-                            $postType= "image";
-                            $targetFile = $_FILES["image"]["name"];
-                            //upload image file
-                            $parts = explode(".", $targetFile);
-                            $extension = end($parts);
-                            $fileName = $communityID . "-" . $postID . "." .$extension;
-                            $tempFile = $_FILES["image"]["tmp_name"];
-                            $destination = "../posts/" . $fileName;
-                            if(copy($_FILES["image"]["tmp_name"], $destination)){
-                                echo "The image was uploaded and moved successfully!";
-                            } else {
-                                echo "There was a problem uploading the file";
-                            }
+                $communityId=0;
+                if($result = mysqli_stmt_get_result($statement)){
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        $communityId = $row['communityId'];
+                    }
+                }
+                $sql = "INSERT INTO posts (communityId, userId, postTime) VALUES (?, 1, NOW())";
+                if ($statement = mysqli_prepare($conn, $sql)) {
+                    mysqli_stmt_bind_param($statement, 'i', $communityId);
+                    mysqli_stmt_execute($statement);
+                    $postType="";
+                    $fileName="";
+                    $postID = mysqli_insert_id($conn);
+                    if($postDesc){
+                        $fileName = "$communityId" . "-" . "$postID.txt";
+                        $postType="text";
+                        $filePath = "../posts/$fileName";
+                        //Upload Text post
+                        $newFile = fopen("$filePath", "w");
+                        if ($newFile) {
+                            fwrite($newFile, $postDesc);
+                            fclose($newFile);
+                            echo "Post created!";
+                        } else {
+                            echo "Error creating file!";
                         }
-                        
-                        $sql = "UPDATE posts SET postDesc=?, postType=? WHERE postId=?";
-                        if ($statement = mysqli_prepare($conn, $sql)) {
-                            mysqli_stmt_bind_param($statement, "ssi", $fileName, $postType, $postID);
-                            mysqli_stmt_execute($statement);
-                            echo "Successfully Posted!";
-                        }else {
-                            echo "Error inserting post: " . mysqli_error($conn);
+                        $fileName = "$communityId" . "-" . "$postID.txt";
+                    } 
+                    else{
+                        $postType= "image";
+                        $targetFile = $_FILES["image"]["name"];
+                        //upload image file
+                        $parts = explode(".", $targetFile);
+                        $extension = end($parts);
+                        $fileName = $communityId . "-" . $postID . "." .$extension;
+                        $tempFile = $_FILES["image"]["tmp_name"];
+                        $destination = "../posts/" . $fileName;
+                        if(copy($_FILES["image"]["tmp_name"], $destination)){
+                            echo "The image was uploaded and moved successfully!";
+                        } else {
+                            echo "There was a problem uploading the file";
                         }
                     }
+                    $sql = "UPDATE posts SET postType=? WHERE postId=?";
+                    if ($statement = mysqli_prepare($conn, $sql)) {
+                        mysqli_stmt_bind_param($statement, "si", $postType, $postID);
+                        mysqli_stmt_execute($statement);
+                        echo "Successfully Posted!";
+                    }else {
+                        echo "Error inserting post: " . mysqli_error($conn);
+                    }
+                }else{
+                    echo "Error with storing first info.";
                 }
             }
             else{
-                echo "Query not done.";
+                echo "Couldn't find the community.";
             }
-    
-        }   
+        }else{
+            echo "Nothing posted.";
+        }
+    }else{
+        echo "No request.";
     }
     ?>
     <a href="index.php">Go back to main page</a>
