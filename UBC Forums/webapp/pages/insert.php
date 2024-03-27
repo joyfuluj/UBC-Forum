807093589f46a161a45b07de5776dcc382aff160
joyfuluj <joyumejiego@yahoo.com>
2024-03-27 13:07:36 -0700

    changed connection in insert.php

--- UBC Forums/webapp/pages/insert.php
+++ UBC Forums/webapp/pages/insert.php
@@ -2,7 +2,17 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-    include('connection.php');
+    $db_hostname = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $db_name = "db_81265373";
+
+    $conn = mysqli_connect($db_hostname, $db_username, $db_password, $db_name);
+
+    if ($conn->connect_error) 
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
     
     function customError($errno, $errstr) {
         echo "<b>Error:</b> [$errno] $errstr";
