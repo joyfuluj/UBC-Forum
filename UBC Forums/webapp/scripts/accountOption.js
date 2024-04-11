3425112c96adbe19926f8816c7a150088b45c96d
Adam Badry <57472142+Sk3tch7y@users.noreply.github.com>
2024-04-11 14:22:22 -0700

    final script added for accounts page

--- UBC Forums/webapp/scripts/accountOption.js
+++ UBC Forums/webapp/scripts/accountOption.js
@@ -1,4 +1,11 @@
 let optionOpen = false;
+
+window.addEventListener('load', function() {
+    if (window.location.search.includes("searchType=")) {
+        toggleOptions();
+    }
+});
+
 function toggleOptions() {
     if (optionOpen) {
         document.getElementById("user_info").style.display = "none";
